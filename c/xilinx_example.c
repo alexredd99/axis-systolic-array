99f7a37ae1e2ba6cfc116a6d99ab1535bf6fa0f1
Aba <abarajithan-7@gmail.com>
2025-04-09 17:11:21 -0700

    Fix xil_example.c

--- c/xilinx_example.c
+++ c/xilinx_example.c
@@ -16,6 +16,11 @@
 
 #define K 16
 
+// Make sure this matches RTL & TCL
+#define CONFIG_BASEADDR 0xB0000000
+#define R 8
+#define C 4
+
 static inline void flush_cache(void *addr, uint32_t bytes) {
   Xil_DCacheFlushRange((INTPTR)addr, bytes);
 }
