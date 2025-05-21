77e5d311538bd525725c1ff7ffb1e2a88ec1dd94
Alexander Redding <alexredd99@gmail.com>
2025-05-21 22:10:07 +0000

    Updated synthesis script

--- tcl/synth.tcl
+++ tcl/synth.tcl
@@ -5,35 +5,49 @@ plugin -i slang
 # Import commands from plugins to tcl interpreter
 yosys -import
 
-set script_location [file dirname [file normalize [info script]]]
-set rtl_path "$script_location/../rtl/axis_sa.sv"
+set tcl_dir [file dirname [file normalize [info script]]]
 
-set top_module axis_sa
+array set opt {
+  top_module      axis_sa
+  rtl_path        ""
+  output_dir      synth_output
+  params          {}
+}
+set opt(rtl_path) "$tcl_dir/../rtl/axis_sa.sv"
 
-set new_params [list]
-foreach param [lrange $argv 0 end] {
-  lappend new_params -G $param
-  puts "Setting $param"
+# Helper to export netlist
+proc export_synth {output_dir} {
+  file mkdir $output_dir
+  write_json $output_dir/synth.json
 }
 
-eval read_slang $rtl_path --top $top_module $new_params
+
+# Parse arguments
+foreach arg $::argv {
+  # Match pattern: key=value
+  if {[regexp {(\w+)=([^ ]+)} $arg -> key val]} {
+    if {$key eq "output_dir"} {
+      set $opt($key) $val
+    } else {
+      lappend opt(params) -G$key=$val
+    }
+  }
+}
+
+# Read RTL
+eval read_slang $opt(rtl_path) --top $opt(top_module) $opt(params)
 
 procs;;
 clean -purge
 setundef -zero
-
-proc export_synth {output_path} {
-  file mkdir $output_path
-  write_json $output_path/synth.json
-}
-
 flatten
-puts [export_synth {synth_output/0_proc}]
+
+# Export process netlist
+export_synth "$opt(output_dir)/0_proc"
 
 # Synthesize design
-synth -top $top_module
+synth
 clean -purge
 autoname
 setundef -zero
-
-puts [export_synth {synth_output/1_synth}]
+export_synth "$opt(output_dir)/1_synth"
